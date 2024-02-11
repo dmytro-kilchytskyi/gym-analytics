7ba867f66d648a0c7a23728b620da78dd8b51ce1
Dmytro Kilchytskyi <d.kilchytskyi@gmail.com>
2024-02-11 17:38:31 +0100

    feat: initial commit

---
+++ src/main/java/com/dkilchytskyi/gymanalytics/GymAnalyticsApplication.java
@@ -0,0 +1,13 @@
+package com.dkilchytskyi.gymanalytics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymAnalyticsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GymAnalyticsApplication.class, args);
+	}
+
+}
